eb5a1d4e25bc6e2816fa174a89de411a8a256880
Mina Gadallah <minamonir11@hotmail.com>
2016-06-03 10:30:33 -0400

    Simple connection and query
    in this java script file I initiate a connection between mySQL server and the computer. 
    I check if the connection is established, and then we preform the queries.

---
+++ jsql01.js
@@ -0,0 +1,29 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection(
+    {
+  		host     : 'www.db4free.net',
+  		user     : 'monmon_2007',
+  		password : 'marina_zoom',
+  		database : 'mina'
+    }
+);
+
+connection.connect(function(err){
+if(!err) {
+    console.log("Database is connected ... nn");
+} else {
+    console.log("Error connecting database ... nn");
+}
+});
+
+
+connection.query('SELECT first FROM info;', function(err, rows, fields) {
+    if (err) {console.log('Error while performing Query.');}
+
+    for (var i in rows) {
+        console.log('First Name: ', rows[i].first);
+    }
+});
+
+connection.end();
