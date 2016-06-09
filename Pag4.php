d1d948420cd4298ee949a0faa055d04cced66229
Mina Gadallah <minamonir11@hotmail.com>
2016-06-09 11:49:58 -0400

    Update Pag4.php

--- Pag4.php
+++ Pag4.php
@@ -14,85 +14,140 @@
 		<script src="app.js"></script>
 	</head>
 
-	<body ng-app="hackRU">
-		<div class="col-md-6 col-md-offset-3">
-			<div class="page-header">
-				<h1>Client Form</h1>
-			</div>
+   <body>
+      <?php
+         if(isset($_POST['add']))
+         {
+            $dbhost = '*************';
+            $dbuser = '*************';
+            $dbpass = '*************';
+	    $dbname = '*************';
+            $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname); // Create connection
 
-			<div class="row col-md-offset-0">
+            if(! $conn )
+            {
+               die('Could not connect: ' . mysql_error());
+            }
+
+            if(! get_magic_quotes_gpc() )
+            {
+               $emp_name = addslashes ($_POST['emp_name']);
+               $emp_address = addslashes ($_POST['emp_address']);
+               $emp_address1 = addslashes ($_POST['emp_address1']);
+               $emp_address2 = addslashes ($_POST['emp_address2']);
+               $emp_address3 = addslashes ($_POST['emp_address3']);
+               $emp_address0 = addslashes ($_POST['emp_address0']);
+            }
+            else
+            {
+               $emp_name = $_POST['emp_name'];
+               $emp_address = $_POST['emp_address'];
+            }
+
+
+            $sql ="INSERT INTO info (first, last, adress, city, state, zip)
+			VALUES ('$emp_name', '$emp_address', '$emp_address0', '$emp_address1', '$emp_address2', '$emp_address3')";
+
+
+            $retval = $conn->query($sql);
+
+            if(! $retval )
+            {
+               die('Could not enter data: ' . $conn->connect_error);
+            }
+				header("Location: http://www.google.com");
+            //echo "Entered data successfully\n";
+
+            $conn->close();
+         }
+         else
+         {
+            ?>
+
+<!-- HTML CODE STARTS HERE -->
+            <div class="col-md-6 col-md-offset-3">
+		<div class="page-header">
+				<h1>Client Form</h1>
+		</div>
+
+		<div class="row col-md-offset-0">
 			Please enter your information to place your job.
 			<br /><br />
+		</div>
+
+		<form ng-submit="addOffer()" method = "post" action = "<?php $_PHP_SELF ?>">
+
+		<!-- FIRST AND LAST NAME -->
+		<div class="row">
+			<div class="col-md-6">
+				<input type="text" input name="emp_name" class="form-control" placeholder="First Name" ng-model="first" id = "emp_name"/>
 			</div>
 
-			<form ng-submit="addOffer()">
-
-			<!-- FIRST AND LAST NAME -->
-			<div class="row">
-				<div class="col-md-6">
-					<input type="text" class="form-control" placeholder="First Name" ng-model="first" />
-				</div>
-
-				<div class="col-md-6">
-					<input type="text" class="form-control" placeholder="Last Name" ng-model="last" />
-				</div>
+			<div class="col-md-6">
+				<input type="text" input name="emp_address" class="form-control" placeholder="Last Name" ng-model="last" id = "emp_address" />
 			</div>
+		</div>
 
-			<!-- ADDRESS -->
+		<!-- ADDRESS -->
 			<div class="row">
 				<div class="col-md-5">
 					<span style="margin-top: 20px; margin-bottom: 10px; display: block;">Address:</span>
-					<input type="text" class="form-control" placeholder="Street Address" ng-model="street" />
+					<input type="text" input name="emp_address0" class="form-control" placeholder="Street Address" ng-model="street" id = "emp_address0" />
 				</div>
 
-				<div class="col-md-3" style="margin-top:50px;">
-					<input type="text" class="form-control" placeholder="City" ng-model="city" />
-				</div>
-
-				<div class="col-md-2" style="margin-top:50px;">
-					<input type="text" class="form-control" placeholder="State" ng-model="state" />
+					<div class="col-md-3" style="margin-top:50px;">
+					<input type="text" input name="emp_address1" class="form-control" placeholder="City" ng-model="city" id = "emp_address1" />
 				</div>
 
 				<div class="col-md-2" style="margin-top:50px;">
-					<input type="text" class="form-control" placeholder="Zip Code" ng-model="zip" />
+					<input type="text" input name="emp_address2" class="form-control" placeholder="State" ng-model="state" id = "emp_address2" />
+				</div>
+
+				<div class="col-md-2" style="margin-top:50px;">
+					<input type="text"  input name="emp_address3" class="form-control" placeholder="Zip Code" ng-model="zip" id = "emp_address3" />
 				</div>
 			</div>
 
-        <div class="row">
-          <div class="col-md-5 " style="margin-top:30px;">
-            <p>Choose one from the following:</p>
-          <select>
+			        <div class="row">
+			          <div class="col-md-5 " style="margin-top:30px;">
+			            <p>Choose one from the following:</p>
+			          <select>
 
-            <option type="text" class="form-control" placeholder="None" ng-model="none" />Select From the folowing</option>
-            <option type="text" class="form-control" placeholder="HouseWare" ng-model="house" />HouseWare</option>
-            <option type="text" class="form-control" placeholder="Car Problem" ng-model="car" />Car Problem</option>
-            <option type="text" class="form-control" placeholder="Technology" ng-model="tech" />Technology</option>
-            <option type="text" class="form-control" placeholder="Other" ng-model="other" />Other</option>
+			            <option type="text" class="form-control" placeholder="None" ng-model="none" />Select From the folowing</option>
+			            <option type="text" class="form-control" placeholder="HouseWare" ng-model="house" />HouseWare</option>
+			            <option type="text" class="form-control" placeholder="Car Problem" ng-model="car" />Car Problem</option>
+			            <option type="text" class="form-control" placeholder="Technology" ng-model="tech" />Technology</option>
+			            <option type="text" class="form-control" placeholder="Other" ng-model="other" />Other</option>
 
-          </select>
-        </div>
-        </div>
+			          </select>
+			        </div>
+			        </div>
 
-			<div class="row">
-				<div class="col-md-12">
-					<span style="margin-top: 20px; margin-bottom: 10px; display: block;">Problem Description:</span>
-					<textarea class="form-control" rows="5"></textarea>
-				</div>
-			</div>
+						<div class="row">
+							<div class="col-md-12">
+								<span style="margin-top: 20px; margin-bottom: 10px; display: block;">Problem Description:</span>
+								<textarea class="form-control" rows="5"></textarea>
+							</div>
+						</div>
 
-      <div class="row">
-      				<div class="col-md-2" style="margin-top: 20px;">
-      					<button type="submit" class="btn btn-primary" ng-click="submit">Submit Offer</button>
-      				</div>
+			      <div class="row">
+			      				<div class="col-md-2" style="margin-top: 20px;">
+			      					<button type="submit" name = "add" class="btn btn-primary" id = "add"
+                              value = "Submit" ng-click="submit">Submit Offer </button>
+			      				</div>
+			      				<div class="col-md-3" style="margin-top: 20px;">
+			      					<button type="reset" class="btn btn-primary" ng-click="clear">Clear Form</button>
+			      				</div>
+			      			</div>
 
-      				<div class="col-md-3" style="margin-top: 20px;">
-      					<button type="reset" class="btn btn-primary" ng-click="clear">Clear Form</button>
-      				</div>
-      			</div>
-
-				</form>
+							</form>
 
 
 		</div>
-	</body>
+
+            <?php
+         }
+      ?>
+
+   </body>
 </html>
